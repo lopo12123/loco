6268d1d91467fbbe68d525352c15ebe70b1f6f7f
lopo <lopo@zju.edu.cn>
2022-04-13 15:02:45 +0800

    feat: loading info box

---
+++ src/renderer/src/components/Misc/LoadingInfo.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+import { withDefaults } from "vue";
+
+const props = withDefaults(defineProps<{
+    text?: string
+}>(), {
+    text: 'loading'
+})
+</script>
+
+<template>
+    <div class="loading-info">
+        <i class="pi pi-sync pi-spin"/>
+        <span class="blink">
+            {{ props.text }}
+        </span>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use "../../styles/mixin";
+
+.loading-info {
+    position: relative;
+    min-width: 100px;
+    width: fit-content;
+    height: 30px;
+    user-select: none;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+
+    i {
+        margin-right: 5px;
+        font-size: 12px;
+    }
+
+    .blink {
+        @include mixin.doBlink(3s);
+        position: relative;
+        width: fit-content;
+        display: inline-block;
+    }
+}
+</style>

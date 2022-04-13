b4f7232b3f692c1be35ed9891d9c1d1807a55518
lopo <lopo@zju.edu.cn>
2022-04-13 15:39:17 +0800

    feat: filereceiver (get path of file dropped)

---
+++ src/renderer/src/components/Misc/FileReceiver.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+import { onMounted, onUnmounted, ref } from "vue";
+import { useToastStore } from "@renderer/stores/store_toast";
+
+const emits = defineEmits<{
+    (event: 'file-drop', val: string): void
+}>()
+
+const containerRef = ref<HTMLDivElement | null>(null)
+
+const dropListener = (e: DragEvent) => {
+    e.preventDefault()
+
+    const files = e.dataTransfer.files
+    if(files && files.length > 0) {
+        // @ts-ignore
+        const path = files[0].path as string
+        if(!path) {
+            useToastStore().warn('Electron only')
+        }
+        else {
+            emits('file-drop', path)
+        }
+    }
+}
+const dragOverListener = (e: DragEvent) => {
+    e.preventDefault()
+}
+
+onMounted(() => {
+    console.log(containerRef.value)
+    containerRef.value?.addEventListener('drop', dropListener)
+    containerRef.value?.addEventListener('dragover', dragOverListener)
+})
+onUnmounted(() => {
+    containerRef.value?.removeEventListener('drop', dropListener)
+    containerRef.value?.removeEventListener('dragover', dragOverListener)
+})
+</script>
+
+<template>
+    <div class="file-receiver" ref="containerRef"></div>
+</template>
+
+<style lang="scss" scoped>
+.file-receiver {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+</style>

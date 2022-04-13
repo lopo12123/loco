7d7a671e6d755cf7f626668450f6d7aa18d539e5
lopo <lopo@zju.edu.cn>
2022-04-13 13:08:41 +0800

    chore: template

--- src/renderer/src/components/Versions.vue
+++ src/renderer/src/components/Versions.vue
@@ -2,13 +2,15 @@
 import { reactive } from 'vue'
 
 const versions = reactive({ ...window.electron.process.versions })
+
+console.log(window.electron.process)
 </script>
 
 <template>
   <ul class="versions">
     <li class="electron-version">Electron v{{ versions.electron }}</li>
     <li class="chrome-version">Chromium v{{ versions.chrome }}</li>
-    <li class="node-version">Node v{{ versions.electron }}</li>
+    <li class="node-version">Node v{{ versions.node }}</li>
     <li class="v8-version">V8 v{{ versions.node }}</li>
   </ul>
 </template>

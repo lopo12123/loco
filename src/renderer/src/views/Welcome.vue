a5fb4101c6fd854279864c7a5f732fb3c3fd89ef
lopo <lopo@zju.edu.cn>
2022-04-13 13:55:26 +0800

    feat: entry view

---
+++ src/renderer/src/views/Welcome.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="welcome">
+        welcome
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.welcome {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+</style>

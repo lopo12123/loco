a8e05dd4483fefd79932fa9af71020c454fde371
lopo <lopo@zju.edu.cn>
2022-04-14 15:23:36 +0800

    refactor: drop ipc declare

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,14 +1,7 @@
-import { contextBridge, ipcRenderer } from "electron";
+import { contextBridge } from "electron";
 import { electronAPI } from "@electron-toolkit/preload";
-import { CB_Renderer } from "../common/declare";
 
 // Custom APIs for renderer
-
-const ipc: CB_Renderer = {
-    banner: (type) => {
-        ipcRenderer.send('banner', { type })
-    }
-}
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
@@ -16,7 +9,6 @@ const ipc: CB_Renderer = {
 if(process.contextIsolated) {
     try {
         contextBridge.exposeInMainWorld('electron', electronAPI)
-        contextBridge.exposeInMainWorld('ipc', ipc)
     }
     catch (error) {
         console.error(error)
@@ -24,5 +16,4 @@ if(process.contextIsolated) {
 }
 else {
     window.electron = electronAPI
-    window.ipc = ipc
 }

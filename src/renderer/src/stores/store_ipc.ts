5876a1fd5d1fd71bf2b6992edaf8ff412d37a45d
lopo <lopo@zju.edu.cn>
2022-04-14 14:39:16 +0800

    feat: type declare

--- src/renderer/src/stores/store_ipc.ts
+++ src/renderer/src/stores/store_ipc.ts
@@ -1,6 +1,6 @@
-import { Channel } from "../../../common/declare";
+import { CB_Renderer, Channel } from "../../../common/declare";
 
-const bannerIpc = window.ipc[Channel.banner]
+const bannerIpc = window.ipc[Channel.banner] as CB_Renderer[Channel.banner]
 
 
 

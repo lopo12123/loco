a8b88d76413a951a9efcf4fa548cff8e6fa6cd38
lopo <lopo@zju.edu.cn>
2022-04-14 18:05:17 +0800

    feat: use git

---
+++ src/main/GitLocal/index.ts
@@ -0,0 +1,66 @@
+import simpleGit, { SimpleGit, StatusResult } from "simple-git";
+import { join } from "path";
+
+class Git {
+    #git: SimpleGit | null = null
+
+    init(path: string) {
+        if(path.endsWith('.git')) path = join(path, '..')
+        return new Promise<Git>((resolve, reject) => {
+            this.#git = simpleGit(path)
+            this.#git.checkIsRepo((err, ifRepo) => {
+                if(err) reject(err)
+                else if(ifRepo) resolve(this)
+                else {
+                    this.#git = null
+                    reject('Not a git repository (or any of the parent directories).')
+                }
+            })
+        })
+    }
+
+    cmd_log() {
+        return new Promise((resolve, reject) => {
+            if(!this.#git) reject('Git has not been initialized.')
+            else this.#git.log([], (err, data) => {
+                err ? reject(err) : resolve(data)
+            })
+        })
+    }
+
+    cmd_status() {
+        return new Promise<StatusResult>((resolve, reject) => {
+            if(!this.#git) reject('Git has not been initialized.')
+            else this.#git.status((err, data) => {
+                err ? reject(err) : resolve(data)
+            })
+        })
+    }
+}
+
+const _ = new Git()
+export const useGit = () => _
+
+// test: log
+// console.time('log')
+// _.init('D:\\GitProjects\\git-loco\\.git')
+//     .then((self) => {
+//         return self.cmd_log()
+//     })
+//     .then((res) => {
+//         console.timeEnd('log')
+//         console.log(res)
+//     })
+//     .catch((e) => {
+//         console.log(e)
+//     })
+
+// test: status
+// _.init('.')
+//     .cmd_status()
+//     .then((res) => {
+//         console.log(res)
+//     })
+//     .catch((e) => {
+//         console.log(e)
+//     })

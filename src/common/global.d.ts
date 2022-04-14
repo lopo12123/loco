db34ba46950c2a1df71ee8265124aabb93f7b573
lopo <lopo@zju.edu.cn>
2022-04-14 15:23:13 +0800

    refactor: drop

--- src/common/global.d.ts
+++ src/common/global.d.ts
@@ -1,9 +1,7 @@
 import { ElectronAPI } from '@electron-toolkit/preload'
-import { CB_Renderer } from "./declare"
 
 declare global {
     interface Window {
         electron: ElectronAPI
-        ipc: CB_Renderer
     }
 }

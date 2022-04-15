386d406708593693d76adb3fb068a4d44edb6d0f
lopo <lopo@zju.edu.cn>
2022-04-15 18:05:38 +0800

    feat: initializer

--- src/renderer/src/views/Initializer.vue
+++ src/renderer/src/views/Initializer.vue
@@ -4,13 +4,24 @@ import { useGitStore } from "../stores/store_git";
 import { ref } from "vue";
 import LoadingInfo from "../components/Misc/LoadingInfo.vue";
 import { useIpcRenderer } from "../stores/store_ipc";
+import { useToastStore } from "../stores/store_toast";
 
 const router = useRouter()
 const gitVersion = useGitStore().useGitVersion()
 
 type DetectStage = 'detecting' | 'unavailable' | 'available'
 const detectStage = ref<DetectStage>('detecting')
-console.log(gitVersion)
+
+const downloadGit = () => {
+    const url = 'https://git-scm.com/downloads'
+    useIpcRenderer().send('browser', { urlToOpen: url })
+    useIpcRenderer().once('browserReply', (e, [ res, msg ]) => {
+        res
+            ? useToastStore().success(msg)
+            : useToastStore().error(msg)
+    })
+}
+
 // no 'git' available
 if(gitVersion === 'unavailable') {
     detectStage.value = 'unavailable'
@@ -18,8 +29,18 @@ if(gitVersion === 'unavailable') {
 // undetected
 else if(gitVersion === null) {
     useIpcRenderer().send('gitDetect')
-    useIpcRenderer().once('gitDetectReply', (e, [res, msg]) => {
-        console.log(res, msg)
+    useIpcRenderer().once('gitDetectReply', (e, [ res, msg ]) => {
+        if(!res) detectStage.value = 'unavailable'
+        else {
+            detectStage.value = 'available'
+            useGitStore().useGitVersion(msg)
+
+            setTimeout(() => {
+                router.push({
+                    name: 'Starter'
+                })
+            }, 1_000)
+        }
     })
 }
 // detected
@@ -35,10 +56,17 @@ else {
     <div class="initializer">
         <LoadingInfo v-if="detectStage === 'detecting'"
                      :spin="true" icon="pi pi-compass"
-                     text="This seems to be your first use, detecting git availability."/>
-        <LoadingInfo v-if="detectStage === 'unavailable'"
-                     :spin="false" icon="pi"/>
-        <LoadingInfo v-if="detectStage === 'available'"/>
+                     text="This seems to be your first use, detecting git`s availability."/>
+        <div class="box-with-link" v-if="detectStage === 'unavailable'">
+            <LoadingInfo :spin="false" icon="pi pi-exclamation-triangle"
+                         :blink="false" text="git is not available."/>
+            <span class="link" @click="downloadGit">
+                <i class="pi pi-link"/>click me to download git in your browser.
+            </span>
+        </div>
+        <LoadingInfo v-if="detectStage === 'available'"
+                     :spin="false" icon="pi pi-check"
+                     :blink="false" text="git available."/>
     </div>
 </template>
 
@@ -47,5 +75,22 @@ else {
     position: relative;
     width: 100%;
     height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .box-with-link {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        .link {
+            margin-top: 10px;
+            color: #9feaf9;
+            cursor: pointer;
+            user-select: none;
+        }
+    }
 }
 </style>

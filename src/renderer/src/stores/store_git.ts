f2b529c34b5e3b5cdba9368062306502dbd8e125
lopo <lopo@zju.edu.cn>
2022-04-15 11:31:40 +0800

    feat: store git info in sessionstorage sync

---
+++ src/renderer/src/stores/store_git.ts
@@ -0,0 +1,37 @@
+import { StatusResult } from "simple-git";
+
+type StatusInfo = Omit<StatusResult, 'isClean'> | null
+type LogInfo = Omit<StatusResult, 'isClean'> | null
+
+/**
+ * @description sync store using sessionStorage
+ */
+class Store_git {
+    // region log
+    #logInfo: LogInfo = null
+    // endregion
+
+    // region status
+    useStatusInfo(): StatusInfo
+    useStatusInfo(val: StatusInfo): void
+    useStatusInfo(val?: StatusInfo): StatusInfo | void {
+        if(val !== undefined) {
+            sessionStorage.setItem('status-info', val === null ? '' : JSON.stringify(val))
+        }
+        else {
+            try {
+                return JSON.parse(sessionStorage.getItem('status-info') ?? '') as StatusInfo
+            }
+            catch (e) {
+                return null
+            }
+        }
+    }
+
+    // endregion
+
+}
+
+const _ = new Store_git()
+
+export const useGitStore = () => _

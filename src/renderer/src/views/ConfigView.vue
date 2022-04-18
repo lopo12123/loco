d6b42527f9d26cedc439a52134548dfd13410848
lopo <lopo@zju.edu.cn>
2022-04-18 16:47:06 +0800

    feat: config-view

---
+++ src/renderer/src/views/ConfigView.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="config-view">
+        config-view
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.config-view {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+</style>

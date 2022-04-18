4d82d8caf5142631df0204f7f05d29d900dcaa9b
lopo <lopo@zju.edu.cn>
2022-04-18 14:44:40 +0800

    feat: commit history page

---
+++ src/renderer/src/views/CommitHistory.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="commit-history">
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.commit-history {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+</style>

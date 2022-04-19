5179b340d526445b4764ea2439a15fd52d47c60c
lopo <lopo@zju.edu.cn>
2022-04-19 17:41:59 +0800

    feat: gitConfigEdit

--- src/renderer/src/views/ConfigView.vue
+++ src/renderer/src/views/ConfigView.vue
@@ -1,10 +1,33 @@
 <script lang="ts" setup>
 
+import { useRouter } from "vue-router";
+
+const router = useRouter()
+
+/**
+ * @description 返回 git-view 页面
+ */
+const back = () => {
+    router.push({
+        name: 'GitView'
+    })
+}
 </script>
 
 <template>
     <div class="config-view">
-        config-view
+        <div class="banner">
+            <div class="banner-left">
+                <span class="hover-underline">在文件中查看/编辑</span>
+                <i class="iconfont icon-shuaxin" title="获取最新状态"/>
+            </div>
+            <div class="banner-right">
+                <i class="iconfont icon-back" title="返回" @click="back"/>
+            </div>
+        </div>
+        <div class="detail">
+            details
+        </div>
     </div>
 </template>
 
@@ -13,5 +36,53 @@
     position: relative;
     width: 100%;
     height: 100%;
+    font-family: cursive;
+    user-select: none;
+
+    .banner {
+        position: relative;
+        width: 100%;
+        height: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        %btn-base {
+            position: relative;
+            width: 30px;
+            height: 30px;
+            border-radius: 5px;
+            text-align: center;
+            line-height: 30px;
+            cursor: pointer;
+            display: inline-block;
+
+            &:hover {
+                background-color: #cccccc1a;
+            }
+        }
+
+        .banner-left {
+            span {
+                position: relative;
+                cursor: pointer;
+                display: inline-block;
+            }
+            i {
+                @extend %btn-base;
+                margin-left: 5px;
+            }
+        }
+
+        .banner-right {
+            i {
+                @extend %btn-base;
+            }
+        }
+    }
+
+    .detail {
+
+    }
 }
 </style>

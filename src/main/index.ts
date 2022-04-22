c9550ba91d362f20aec3c686e1db53fa989eec7e
lopo <lopo@zju.edu.cn>
2022-04-22 16:01:27 +0800

    fix: min-size limit for the window

--- src/main/index.ts
+++ src/main/index.ts
@@ -10,6 +10,8 @@ const createWindow = () => {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        minWidth: 800,
+        minHeight: 600,
         show: false,
         frame: false,
         icon: join(__dirname, '../../build/icon.ico'),
